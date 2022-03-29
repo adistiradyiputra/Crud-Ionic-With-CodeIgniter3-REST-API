c68eafcd7f012b87dd3cb471276586da8c5da9a9
adistiradyiputra <adistiraxx77@gmail.com>
2022-03-29 23:25:56 +0700

    First Upload Tugas IPAT

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+
+export class Tab1Page {
+  public getMahasiswa: any;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  ionViewWillEnter() {
+    this._Getdata();
+  }
+
+  
+  _Getdata() {
+    let data: Observable<any>;
+    data = this.http.get('http://localhost/CodeIgniter_API/index.php/api/GetData');
+    data.subscribe(result => {
+      console.log(result);
+      this.getMahasiswa = result;
+    });
+  }
+ 
+
+
+
+
+}
+
+
+
+
